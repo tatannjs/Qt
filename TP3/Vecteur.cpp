af632b2e3b6b5995eff65aeaed65bede60946477
Ethan Eveilleaux <i2300764@cri.univ-lemans.fr>
2025-10-08 16:30:24 +0200

    [WIP] TP3

--- TP3/Vecteur.cpp
+++ TP3/Vecteur.cpp
@@ -6,17 +6,129 @@ using namespace std;
 
 #include "Vecteur.h"
 
-Vecteur::Vecteur() {
+// TODO : Problème de gestion de mémoire du tab
+Vecteur::Vecteur()
+{
+    this->n = 0;
+    this->t = NULL;
 }
 
-Vecteur::Vecteur(int n, long double valInit) {
+Vecteur::Vecteur(int n, long double valInit)
+{
+    this->n = n;
+    this->t = new long double[n];
+    for (int i = 0; i < n; i++)
+    {
+        this->t[i] = valInit;
+    }
 }
 
-Vecteur::Vecteur(int n, long double *t) {
+Vecteur::Vecteur(int n, long double *t)
+{
+    this->n = n;
+    this->t = new long double[n];
+    for (int i = 0; i < n; i++)
+    {
+        this->t[i] = t[i];
+    }
 }
 
-Vecteur::~Vecteur() {
+Vecteur::~Vecteur()
+{
+    delete[] t;
 }
 
-Vecteur::Vecteur(const Vecteur & v) {
+Vecteur::Vecteur(const Vecteur &v)
+{
+    this->n = v.n;
+    this->t = v.t;
+}
+
+int Vecteur::size() const
+{
+    return this->n;
+}
+
+Vecteur &Vecteur::operator=(const Vecteur &v)
+{
+    if (this != &v)
+    {
+        this->n = v.n;
+        this->t = v.t;
+    }
+    return *this;
+}
+
+long double Vecteur::operator[](unsigned int i) const
+{
+    return this->t[i];
+}
+
+long double &Vecteur::operator[](unsigned int i)
+{
+    return this->t[i];
+}
+
+long double Vecteur::norme()
+{
+    long double somme = 0.0;
+    for (int i = 0; i < this->n; i++)
+    {
+        somme += t[i] * t[i];
+    }
+    return sqrtl(somme);
+}
+
+void Vecteur::normalise()
+{
+    long double norme = this->norme();
+    for (int i = 0; i < this->n; i++)
+    {
+        this->t[i] = this->t[i] / norme;
+    }
+}
+
+ostream &operator<<(ostream &os, const Vecteur &v)
+{
+    os << "<" << v.n << ":";
+    for (int i = 0; i < v.n; i++)
+    {
+        os << v.t[i];
+        if (i < v.n - 1)
+        {
+            os << ",";
+        }
+    }
+    os << ">";
+    return os;
+}
+
+Vecteur operator-(const Vecteur &v)
+{
+    long double *t2 = new long double[v.n];
+    for (int i = 0; i < v.n; i++)
+    {
+        t2[i] = -v.t[i];
+    }
+    Vecteur result(v.n, t2);
+    delete[] t2;
+    return result;
+}
+
+Vecteur operator*(const Vecteur &v, const long double x)
+{
+    long double *t2 = new long double[v.n];
+    for (int i = 0; i < v.n; i++)
+    {
+        t2[i] = v.t[i] * x;
+    }
+
+    Vecteur result(v.n, t2);
+    delete[] t2;
+    return result;
+}
+
+Vecteur operator*(const long double x, const Vecteur &v)
+{
+    return v * x;
 }
--- TP3/testVecteur.cpp
+++ TP3/testVecteur.cpp
@@ -11,168 +11,171 @@ long double f1(long double);
 
 long double f2(long double, long double);
 
-int main() {
+int main()
+{
    long double t2[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
    long double t3[5] = {2.0, 3.0, 4.0, 5.0, 6.0};
-   
+
    // Constructeurs
    // 1 - Vecteur de dimension quelconque.
-//   Vecteur v0(4, 0.0);
+   Vecteur v0(4, 0.0);
 
    // 2 - Vecteur vide (dimension 0)
-//   Vecteur v1;
- 
+   Vecteur v1;
+
    // 3 - Vecteur contruit à partir d'une dimension et d'un tableau
-//   Vecteur v2(5, t2);
-//   Vecteur v3(5, t3);
+   Vecteur v2(5, t2);
+   Vecteur v3(5, t3);
 
    // 4 - Constructeur de recopie
-//   Vecteur v4(v2);
+   Vecteur v4(v2);
 
    // Précicion d'affichage des réels avec 2 chiffres après la virgule.
    cout.precision(2);
 
    // 7 - Opérateur de flux en sortie
    cout << "1 - Vecteur de dimension quelconque" << endl;
-//   cout << "V0 = " << v0 << endl << endl;
+   cout << "V0 = " << v0 << endl
+        << endl;
 
    cout << "2 - Vecteur vide" << endl;
-//   cout << "V1 = " << v1 << endl << endl;
+   cout << "V1 = " << v1 << endl
+        << endl;
 
    cout << "3 - Vecteur construit à partir d'une dimension et d'un tableau" << endl;
-//   cout << "V2 = " << v2 << endl;
-//   cout << "V3 = " << v3 << endl << endl;
+   cout << "V2 = " << v2 << endl;
+   cout << "V3 = " << v3 << endl
+        << endl;
 
    cout << "4 - Constructeur de recopie (v4 à partir de v2)" << endl;
-//   cout << "V4 = " << v4 << endl << endl;
+   cout << "V4 = " << v4 << endl
+        << endl;
 
    cout << "6 - Taille d'un vecteur" << endl;
-//   cout << "V3 : " << v3.size() << endl << endl;
+   cout << "V3 : " << v3.size() << endl
+        << endl;
 
    // 8 - Affectation
    cout << "8 - Affectation v0 = v2" << endl;
-/*
    v0 = v2;
-   cout << "V0 = " << v0 << endl << endl;
-*/
+   cout << "V0 = " << v0 << endl
+        << endl;
 
    // 9 - Indexation
    cout << "9 - Indexation v0[0]" << endl;
-//   cout << "V0[0] = " << v0[0] << endl;
+   cout << "V0[0] = " << v0[0] << endl;
    cout << "9 - Indexation V0[0] = V0[3]" << endl;
-/*
    v0[0] = v0[3];
-   cout << "v0[0] = " << v0[0] << endl << endl;
-*/
+   cout << "v0[0] = " << v0[0] << endl
+        << endl;
 
    // 10 - Norme
    cout << "10 - Norme de V0" << endl;
-/*
    cout << "V0 = " << v0 << endl;
-   cout << "Norme de V0 = " << v0.norme() << endl << endl;
-*/
+   cout << "Norme de V0 = " << v0.norme() << endl
+        << endl;
 
    // 11 - Normalisation
    cout << "11 - Norme de V0 (après normalisation)" << endl;
-/*
    v0.normalise();
    cout << "V0 = " << v0 << endl;
-   cout << "Norme de V0 = " << v0.norme() << endl << endl;
-*/
+   cout << "Norme de V0 = " << v0.norme() << endl
+        << endl;
 
-    // 12 - Multiplication d'un vecteur par un scalaire
+   // 12 - Multiplication d'un vecteur par un scalaire
    cout << "12 - Multiplication d'un vecteur par un scalaire" << endl;
-/*
    cout << "V2 * 2 = " << v2 * 2 << endl;
-   cout << "2 * V2 = " << 2 * v2 << endl << endl;
-*/
+   cout << "2 * V2 = " << 2 * v2 << endl
+        << endl;
 
    // 13 - Opérateur - unaire
    cout << "13 - Opérateur - unaire" << endl;
-//   cout << "-V1 = " << -v2 << endl << endl;
+   cout << "-V1 = " << -v2 << endl << endl;
 
    // 14 - Addition et soustraction de 2 vecteurs
    cout << "14 - Addition et soustraction de 2 vecteurs" << endl;
-/*
-   cout << "V2 = " << v2 << endl;
-   cout << "V3 = " << v3 << endl;
-   cout << "V2 + V3 = ";
-   try {
-       cout << (v2 + v3) << endl;
-   }
-   catch (Erreur & e) {
-      cout << e.what() << endl;
-   }
-   cout << "V3 - V2 = ";
-   try {
-       cout << (v3 - v2) << endl << endl;
-   }
-   catch (Erreur & e) {
-      cout << e.what() << endl;
-   }
-*/
+   /*
+      cout << "V2 = " << v2 << endl;
+      cout << "V3 = " << v3 << endl;
+      cout << "V2 + V3 = ";
+      try {
+          cout << (v2 + v3) << endl;
+      }
+      catch (Erreur & e) {
+         cout << e.what() << endl;
+      }
+      cout << "V3 - V2 = ";
+      try {
+          cout << (v3 - v2) << endl << endl;
+      }
+      catch (Erreur & e) {
+         cout << e.what() << endl;
+      }
+   */
 
    // 15 - Application d'une fonction à 1 argument
    cout << "15 - Application d'une fonction à 1 argument" << endl;
-/*
-   cout << "V4 = " << v4 << endl;
-   cout << "V4.appliquer1(f1) = " << v4.appliquer1(f1) << endl << endl;
-*/
+   /*
+      cout << "V4 = " << v4 << endl;
+      cout << "V4.appliquer1(f1) = " << v4.appliquer1(f1) << endl << endl;
+   */
 
    // 16 - Produit scalaire de 2 vecteurs
    cout << "16 - Produit scalaire de 2 vecteurs" << endl;
-/*
-   cout << "V2 * V3 = ";
-   try {
-       cout << (v2 * v3) << endl  << endl;
-   }
-   catch (Erreur & e) {
-      cout << e.what() << endl;
-   }
-*/
+   /*
+      cout << "V2 * V3 = ";
+      try {
+          cout << (v2 * v3) << endl  << endl;
+      }
+      catch (Erreur & e) {
+         cout << e.what() << endl;
+      }
+   */
 
    // 17 - Opérateur de flux en entrée
    cout << "17 - Opérateur de flux en entrée" << endl;
-/*
-   cout << "Entrez un vecteur : " << endl;
-   cin >> v1;
-   if (! cin) {
-      cout << "ERREUR de lecture\n";
-      exit (-1);
-   }
-   cout << "V1 = " << v1 << endl << endl;
-*/
+   /*
+      cout << "Entrez un vecteur : " << endl;
+      cin >> v1;
+      if (! cin) {
+         cout << "ERREUR de lecture\n";
+         exit (-1);
+      }
+      cout << "V1 = " << v1 << endl << endl;
+   */
 
    // 18 - Produit vectoriel de 2 vecteurs
    cout << "18 - Produit vectoriel de 2 vecteurs" << endl;
-/*
-   cout << "Entrez un vecteur V1 : " << endl;
-   cin >> v1;
-   cout << "Entrez un vecteur V2 : " << endl;
-   cin >> v2;
-   cout << "V1 = " << v1 << endl;
-   cout << "V2 = " << v2 << endl;
-   cout << "V1 ^ V2 = " << v1 << " ^ " << v2 << " = " << (v1 ^ v2) << endl << endl;
-*/
+   /*
+      cout << "Entrez un vecteur V1 : " << endl;
+      cin >> v1;
+      cout << "Entrez un vecteur V2 : " << endl;
+      cin >> v2;
+      cout << "V1 = " << v1 << endl;
+      cout << "V2 = " << v2 << endl;
+      cout << "V1 ^ V2 = " << v1 << " ^ " << v2 << " = " << (v1 ^ v2) << endl << endl;
+   */
 
    // 19 - Application d'une fonction à 2 argument
    cout << "19 - Application d'une fonction à 2 arguments" << endl;
-/*
-   try {
-      cout << "appliquer2(f2, V1, V2) = " << appliquer2(f2, v1, v2) << endl << endl;
-   }
-   catch(Erreur & e) {
-      cout << e.what() << endl;
-   }
-*/
+   /*
+      try {
+         cout << "appliquer2(f2, V1, V2) = " << appliquer2(f2, v1, v2) << endl << endl;
+      }
+      catch(Erreur & e) {
+         cout << e.what() << endl;
+      }
+   */
 
    return 0;
 }
 
-long double f1(long double x) {
+long double f1(long double x)
+{
    return x / 2;
 }
 
-long double f2(long double x, long double y) {
+long double f2(long double x, long double y)
+{
    return (x + y) / 2;
 }
